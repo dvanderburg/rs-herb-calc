a32f6f002873672859ac6d664f68e493e2e231af
Dave Vanderburg <dave@hitgrab.com>
2017-06-27 09:08:42 -0400

    Added clear button to NavigationSideLink

--- src/containers/Navigation/NavigationSideBar.js
+++ src/containers/Navigation/NavigationSideBar.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { clearAll } from '../../redux/actions/clearAll';
+
+import NavigationIconButton from '../../components/Navigation/NavigationIconButton';
 import NavigationSideLink from '../../components/Navigation/NavigationSideLink';
 
 import './NavigationSideBar.css';
@@ -11,10 +15,13 @@ class NavigationSideBar extends React.Component {
 		return (
 			<div className="navigation-side-bar">
 				<div className="side-bar-header">RS Herb Calc</div>
-				<NavigationSideLink to="/herbs" text="Herbs" />
-				<NavigationSideLink to="/secondaries" text="Secondaries" />
-				<NavigationSideLink to="/output" text="Output" />
-				<NavigationSideLink to="/requirements" text="Requirements" />
+				<NavigationSideLink to="/herbs">Herbs</NavigationSideLink>
+				<NavigationSideLink to="/secondaries">Secondaries</NavigationSideLink>
+				<NavigationSideLink to="/output">Output</NavigationSideLink>
+				<NavigationSideLink to="/requirements">Requirements</NavigationSideLink>
+				<div className="navigation-side-buttons">
+					<NavigationIconButton title="Clear" onClick={this.props.onClearClick} d="M6 32h20l2-22h-24zM20 4v-4h-8v4h-10v6l2-2h24l2 2v-6h-10zM18 4h-4v-2h4v2z" />
+				</div>
 			</div>
 		);
 		
@@ -22,4 +29,18 @@ class NavigationSideBar extends React.Component {
 	
 }
 
-export default NavigationSideBar;
+function mapStateToProps(state, ownProps) {
+	
+	return {}
+	
+}
+
+function mapDispatchToProps(dispatch) {
+	
+	return {
+		onClearClick: () => dispatch(clearAll())
+	}
+	
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, null, { pure: false })(NavigationSideBar);
